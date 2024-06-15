e7b22a5eef804e8772b5f33720838ae761b75a73
Coralie LOKOSSOU <coralielokossou@gmail.com>
2024-06-15 17:04:35 +0100

    fichier du chat

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express= require('express');
+const mongoose = require('mongoose');
 const app= express();
 const port=3000;
 
-// Liste de tâches (simulée avec un tableau)
- let informations = [];
+//Connexion à MongoDB
+ mongoose.connect('mongodb://localhost/informations', { useNewUrlParser: true, useUnifiedTopology: true });
+
+ // Schéma MongoDB pour les tâches
+     const listeSchema = new mongoose.Schema({
+   nom: String,
+   prenom: String,
+   annee: String
+   });
+
+   // Modèle MongoDB basé sur le schéma
+   const liste = mongoose.model('liste', listeSchema);
+
+
 
  //Middleware pour utiliser JSON dans les requetes
  app.use(express.json());
 
- //Endpoint pour obtenir la liste des taches 
- app.get('/informations',(req, res)=>{
-    res.json(informations);
- });
+ // Endpoint pour obtenir la liste des tâches
+ app.get('/informations', async (req, res) => {
+   try {
+   const informations = await liste.find();
+   res.json(informations);
+   } catch (err) {
+   res.status(500).json({ error: 'Erreur lors de la récupération des tâches' });
+   }
+});
 
  //Endpoint pour ajouter une tache
- app.post('/informations',(req,res)=>{
+ app.post('/informations',async (req,res)=>{
     const {nom,prenom,annee}=req.body;
     if(!nom)  
     {
@@ -29,14 +47,20 @@ const port=3000;
             {
                return res.status(400).json({error:'Veuillez entrer votre année de naissance'});
             }
-const liste ={
+/*const liste ={
     nom,
     prenom,
     annee,
-};
-   informations.push(liste);
-    res.status(201).json({ message: 'Informations ajoutée avec succès' });
- });
+}*/
+try {
+   const newliste = new liste({ nom, prenom, annee });
+   await newliste.save();
+   res.status(201).json({ message: 'informations ajoutée avec succès' });
+   } catch (err) {
+   res.status(500).json({ error: 'Erreur lors de l\'ajout des informations' });
+   }
+   });
+  
 
  // Démarrer le serveur
  app.listen(port, () => {
